f22075c540a3326a1653e6b89d4427d47e3659b4
MatheusVict <matheusvictorhenrique@gmail.com>
2024-04-20 09:59:37 -0300

    fix: fix the swagger does not show up

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+import { resolve } from 'path';
+import { writeFileSync, createWriteStream } from 'fs';
+import { get } from 'https';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
@@ -23,6 +27,42 @@ async function bootstrap() {
   app.listen(port).then(() => {
     const logger = new Logger(bootstrap.name);
     logger.log('Server listening on ' + port);
+  });
+
+  const serverUrl = process.env.SERVER_STATIC_FILES_URL;
+
+  get(
+    `${serverUrl}/swagger/swagger-ui-bundle.js`, function 
+    (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+      console.log(
+  `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
+);
+  });
+
+  get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
+    response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
+    console.log(
+  `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
+);
+  });
+
+  get(
+`${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
+function (response) {
+    response.pipe(
+    createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
+  );
+    console.log(
+    `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
+  );
+  });
+
+  get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
+    response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+    console.log(
+  `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
+);
   });
 }
 bootstrap();
